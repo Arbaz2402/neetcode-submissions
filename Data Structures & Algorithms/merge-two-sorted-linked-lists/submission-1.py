822bc31948898dedf24cd2e1eb9ff8d1f2cf2db0
Arbaz2402 <121999150+Arbaz2402@users.noreply.github.com>
2026-09-16 16:51:08 +0530

    Add: merge-two-sorted-linked-lists - submission-1

---
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-1.py
@@ -0,0 +1,40 @@
+class Solution:
+    def mergeTwoLists(
+        self,
+        list1: Optional[ListNode],
+        list2: Optional[ListNode]
+    ) -> Optional[ListNode]:
+
+        if list1 == None:
+            return list2
+
+        if list2 == None:
+            return list1
+
+        # Find the head of the merged list
+        if list1.val <= list2.val:
+            head = list1
+            list1 = list1.next
+        else:
+            head = list2
+            list2 = list2.next
+
+        current = head
+
+        while list1 != None and list2 != None:
+
+            if list1.val <= list2.val:
+                current.next = list1
+                list1 = list1.next
+            else:
+                current.next = list2
+                list2 = list2.next
+
+            current = current.next
+
+        if list1 != None:
+            current.next = list1
+        else:
+            current.next = list2
+
+        return head
